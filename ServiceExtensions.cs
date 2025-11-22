27e7b34a22712449debd5f06539c4e17229a4e9e
Andy Walter <musictopia2@hotmail.com>
2025-11-21 19:36:37 -0700

    Updated to use new extension model.

--- ServiceExtensions.cs
+++ ServiceExtensions.cs
@@ -1,19 +1,22 @@
 ﻿namespace UpdateManager.AppManagementCoreLibrary;
 public static class ServiceExtensions
 {
-    public static IServiceCollection RegisterShortcutServices(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddSingleton<IShortcutManager, WindowsDesktopShortcutManager>();
-        return services;
-    }
-    public static IServiceCollection RegisterAppServices(this IServiceCollection services, bool useFileBased = true)
-    {
-        if (useFileBased)
+        public IServiceCollection RegisterShortcutServices()
         {
-            services.AddSingleton<IAppsContext, FileAppsContext>();
+            services.AddSingleton<IShortcutManager, WindowsDesktopShortcutManager>();
+            return services;
         }
-        services.AddSingleton<IShortcutManager, WindowsDesktopShortcutManager>()
-            .AddSingleton<AppDiscoveryService>();
-        return services;
+        public IServiceCollection RegisterAppServices(bool useFileBased = true)
+        {
+            if (useFileBased)
+            {
+                services.AddSingleton<IAppsContext, FileAppsContext>();
+            }
+            services.AddSingleton<IShortcutManager, WindowsDesktopShortcutManager>()
+                .AddSingleton<AppDiscoveryService>();
+            return services;
+        }
     }
 }
